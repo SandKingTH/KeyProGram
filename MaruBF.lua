b9167600c481e2ba666a32b119cc29e2f24a5b17
Sanbax <130027581+SandKingTH@users.noreply.github.com>
2025-04-23 00:07:32 +0700

    Update MaruBF.lua

--- MaruBF.lua
+++ MaruBF.lua
@@ -3,28 +3,31 @@ repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 
-if game:GetService("Players").LocalPlayer.PlayerGui["Main (minimal)"].ChooseTeam then
+local player = game:GetService("Players").LocalPlayer
+local playerGui = player:FindFirstChild("PlayerGui")
+local mainGui = playerGui and playerGui:FindFirstChild("Main (minimal)")
+local chooseTeamGui = mainGui and mainGui:FindFirstChild("ChooseTeam")
+
+if chooseTeamGui then
     repeat
         task.wait()
-        local chooseTeamGui = game:GetService("Players").LocalPlayer.PlayerGui["Main (minimal)"].ChooseTeam
-        if game:GetService("Players").LocalPlayer.PlayerGui["Main (minimal)"].ChooseTeam.Visible then
-            for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui["Main (minimal)"].ChooseTeam.Container["Pirates"].Frame.TextButton.Activated)) do
-                for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
+        if chooseTeamGui.Visible then
+            for _, v in pairs(getconnections(chooseTeamGui.Container["Pirates"].Frame.TextButton.Activated)) do
+                for _, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
                     b:Fire() 
                 end
                 v.Function()
             end
         end
-    until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
+    until player.Team ~= nil and game:IsLoaded()
 end
 
-local GodHuman = tonumber(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyGodhuman",true))
+local GodHuman = tonumber(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyGodhuman", true))
 if GodHuman then
     if GodHuman == 1 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/SandKingTH/RawScriptAll/refs/heads/main/Maru_mumu.lua"))()
     end
-    wait(1)
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/SandKingTH/RawScriptAll/refs/heads/main/Banana_mumu.lua"))()
-        
+    task.wait(1)
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SandKingTH/RawScriptAll/refs/heads/main/Banana_mumu.lua"))()
 end
